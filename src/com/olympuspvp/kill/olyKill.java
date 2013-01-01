ca9001589df8460c428663e18062585e620de886
CalDaBeast <cal@The-Pheonix.local>
2013-01-01 16:00:38 -0500

    v1.1.1 - Adding ChatColors and Fixing coding conventions

--- src/com/olympuspvp/kill/olyKill.java
+++ src/com/olympuspvp/kill/olyKill.java
@@ -1,39 +1,40 @@
 package com.olympuspvp.kill;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class olyKill extends JavaPlugin{
+
+	final String tag = ChatColor.DARK_RED + "[" + ChatColor.RED + "olyKill" + ChatColor.DARK_RED + "] " + ChatColor.RED;
+
 	@Override
 	public void onEnable(){
 		System.out.println("[olyKill] olyKill by Medious has been enabled");
 	}
 
-	public boolean onCommand(CommandSender s, Command cmd, String c, String[] args){
-		if (s instanceof Player == false) return true;
-		Player p = (Player)s;
+	@Override
+	public boolean onCommand(final CommandSender s, final Command cmd, final String c, final String[] args){
+		if(s instanceof Player == false) return true;
+		final Player p = (Player)s;
 		if(args.length == 0){
 			p.setHealth(0);
 		}else if(args.length == 1 ){
-			if (p.hasPermission("olyKill.use")== false){
-				p.sendMessage("[olyKill] you do not have permission to use this");
-				return true; 
-			}
-			String name = args[0];
-			Player kill = Bukkit.getPlayer(name);
-			if (kill == null) {
-				p.sendMessage("[olyKill] Could not find player. ");
-				
-			}else {
-				kill.sendMessage("[olyKill] You were killed by "+p.getName());
-				p.sendMessage("[olyKill] you killed "+kill.getName());
+			if(p.hasPermission("olyKill.use")== false){
+				p.sendMessage(tag + "You do not have permission to use this");
+				return true;
+			}final String name = args[0];
+			final Player kill = Bukkit.getPlayer(name);
+			if (kill == null) p.sendMessage(tag + "Could not find player. ");
+			else{
+				kill.sendMessage(tag + "You were killed by " + ChatColor.DARK_RED + p.getName());
+				p.sendMessage(tag + "You killed " + ChatColor.DARK_RED + kill.getName());
 				kill.setHealth(0);
 			}
-		}
-		return true;
+		}return true;
 	}
 
 }
